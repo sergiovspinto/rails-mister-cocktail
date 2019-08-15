2fcad6c5321d5dba9d53fd345431a2eb0ba68870
Sergio Pinto <sergio.pinto.arq@gmail.com>
2019-08-15 19:59:41 +0200

    completed

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,16 +1,16 @@
 class Ingredient < ApplicationRecord
-  has_many :cocktails, through: :doses
   has_many :doses
-  before_destroy :check_for_cocktails
+  has_many :cocktails, through: :doses
+#  before_destroy :check_for_cocktails
   validates :name, presence: true, uniqueness: true
 
-  private
+#  private#
 
-  def check_for_cocktails
-    if cocktails.count.positive?
-      errors.add_to_base('cannot delete ingredient while cocktails exist')
-    else
-      false
-    end
-  end
+#  def check_for_cocktails
+#    if cocktails.count.positive?
+#      errors.add_to_base('cannot delete ingredient while cocktails exist')
+#    else
+#      false
+#    end
+#  end
 end
